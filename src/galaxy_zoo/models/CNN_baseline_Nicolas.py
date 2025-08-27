3728e1492eeee9c833f3722b44b73cef6b7e926b
nicolasmerzeau <nicolas.merzeau@stanfordalumni.org>
2025-08-27 14:15:18 +0200

    Baseline Model Edited

--- src/galaxy_zoo/models/CNN_baseline_Nicolas.py
+++ src/galaxy_zoo/models/CNN_baseline_Nicolas.py
@@ -9,20 +9,17 @@ from keras import optimizers
 from keras.callbacks import EarlyStopping
 from tensorflow.keras.utils import to_categorical
 
+
 import sys, os
 sys.path.append(os.path.abspath("../src"))
-from galaxy_zoo.logic.data import load_data
+from galaxy_zoo.logic.data import generate_image_df, load_and_preprocess_data
 
+df = generate_image_df(nb_data = 2000) # default values
+df
 
-# Import de X et y -> Ici 1000 par categorie
-df_images = load_data(1000)
+X, y = load_and_preprocess_data(df, False, target_size=(424,424))
 
-# Conversion de X et y
-X= df_images["image"]
-X = np.stack(X.to_numpy(), axis = 0)
-y= df_images["label"]
 y_cat= to_categorical(y)
-
 
 # Initialisation d'un modèle
 def initialize_model():
@@ -59,32 +56,33 @@ def initialize_model():
 def compile_model(model):
     model.compile(loss = 'categorical_crossentropy',
                   optimizer = 'adam',
-                  metrics = ['accuracy'])
+                  metrics = ['Precision'])
     return model
 
 #Code pour fit et run le modèle
 
-model_small = initialize_model()
+model = initialize_model()
 model_small = compile_model(model)
 
-es = EarlyStopping(patience = 5, verbose = 2)
+es = EarlyStopping(patience = 5, verbose = 2,restore_best_weights=True)
 
 history_small = model_small.fit(X, y_cat,
                     validation_split = 0.3,
                     callbacks = [es],
                     epochs = 20,
-                    batch_size = 128)
+                    batch_size = 32)
+
 
 # Avec ces paramètres on obtient une val accuracy de 0.68 (pour 1000 par categorie)
 # Avec ces paramètres on obtient une val accuracy de 0.70 (pour 2000 par categorie- batch size 128)
 # Avec ces paramètres on obtient une val accuracy de 0.73 (pour 2000 par categorie- batch size 32)
-
+# Avec ces paramètres on obtient une val precision de 0.78 (pour 2000 par categorie- batch size 32)
 
 
 
 # Fonction pour plot history
 
-def plot_history(history, title='', axs=None, exp_name=""):
+def plot_history2(history, title='', axs=None, exp_name=""):
     if axs is not None:
         ax1, ax2 = axs
     else:
@@ -97,15 +95,17 @@ def plot_history(history, title='', axs=None, exp_name=""):
     ax1.set_ylim(0., 2.2)
     ax1.set_title('loss')
     ax1.legend()
+    ax1.set_xlabel("Epoch")
 
-    ax2.plot(history.history['accuracy'], label='train accuracy'  + exp_name)
-    ax2.plot(history.history['val_accuracy'], label='val accuracy'  + exp_name)
+    ax2.plot(history.history['precision'], label='train precision'  + exp_name)
+    ax2.plot(history.history['val_precision'], label='val precision'  + exp_name)
     ax2.set_ylim(0.25, 1.)
-    ax2.set_title('Accuracy')
+    ax2.set_title('precision')
     ax2.legend()
+    ax2.set_xlabel("Epoch")
     return (ax1, ax2)
 
 # Plot de history
 
-plot_history(history_small)
+plot_history2(history_small)
 plt.show()
