35d2cfbbb8dad5a82437a41fe999df121f02db3b
kaniba keita <kaniba.keita93@gmail.com>
2025-08-28 11:31:10 +0200

    Refactor model comparison logic; streamline model creation and update parameters for improved flexibility

--- src/galaxy_zoo/models/compare_models.py
+++ src/galaxy_zoo/models/compare_models.py
@@ -5,82 +5,70 @@ target_names = {
     0: "ELLIPTICAL",
     1: "SPIRAL",
     2: "EDGE_CIGAR",
+    -1: "ALL",
 }
 params = {
-    'IMG_SIZE': 256,
-    'RANDOM_STATE': 42,
-    'NB_DATA': 100,
+    'IMG_SIZE': [256, 424],
+    'NB_DATA': [100],
     "TEST_SIZE": 0.3,
-    "EPOCHS": 5,
+    "EPOCHS": [3],
 }
 
 models = [
     {
-        "MODEL_NAME": "BASE_LINE_3_CLASSES",
         "MODEL_FUNC": model_small,
         "OVR": False,
-        "TARGET_CLASS": 0,
     },
     {
-        "MODEL_NAME": "BASE_LINE_SPIRAL",
         "MODEL_FUNC": model_small_ovr,
         "OVR": True,
-        "TARGET_CLASS": 0,
-    },
-    {
-        "MODEL_NAME": "BASE_LINE_ELLIPTICAL",
-        "MODEL_FUNC": model_small_ovr,
-        "OVR": True,
-        "TARGET_CLASS": 1,
-    },
-    {
-        "MODEL_NAME": "BASE_LINE_EDGE",
-        "MODEL_FUNC": model_small_ovr,
-        "OVR": True,
-        "TARGET_CLASS": 2,
+        "TARGET_CLASS": [0,1,2],
     },
 
 ]
-def create_model_name(mod, ovr = True, ):
-    if ovr:
-        return f"OVR_TARGET_{target_names[mod['TARGET_CLASS']]}_{mod['MODEL_FUNC'].__name__.upper()}"
+def create_model_name(mod, img_size, nb_img, epochs, target = -1):
+    if mod['OVR']:
+        return f"TARGET_{target_names[target]}_{mod['MODEL_FUNC'].__name__.upper()}_{img_size}_{nb_img}_EPOCHS_{epochs}"
     else:
-        return f"3_CAT_{mod['MODEL_FUNC'].__name__.upper()}"
+        return f"3_CAT_{mod['MODEL_FUNC'].__name__.upper()}_{img_size}_{nb_img}_EPOCHS_{epochs}"
 
 
 def run_models(params=params, models=models):
 
-    input_shape = (params['IMG_SIZE'], params['IMG_SIZE'], 3)
-    nb_data = params['NB_DATA']
-    epochs = params['EPOCHS']
-
     metrics = {}
 
-    for mod in models:
-        model_name = create_model_name(mod, mod['OVR'])
-        if mod['OVR']:
-            res = model_ovr_pipeline(
-                nb_data,
-                mod['TARGET_CLASS'],
-                epochs,
-                mod['MODEL_FUNC'],
-                input_shape,
-                metrics_only = True
-            )
-            metrics[model_name] = res
-        else:
-            res = model_full_pipeline(
-                nb_data,
-                epochs,
-                mod['MODEL_FUNC'],
-                input_shape,
-                metrics_only = True
-            )
-            metrics[model_name] = res
+    for img_size in params['IMG_SIZE']:
+        for nb_data in params['NB_DATA']:
+            input_shape = (img_size, img_size, 3)
+            for epochs in params['EPOCHS']:
+                for mod in models:
+                    if mod['OVR']:
+                        for target in mod['TARGET_CLASS']:
+                            model_name = create_model_name(mod, img_size, nb_data, epochs, target)
+                            res = model_ovr_pipeline(
+                                nb_data,
+                                target,
+                                epochs,
+                                mod['MODEL_FUNC'],
+                                input_shape,
+                                metrics_only = True
+                            )
+                            metrics[model_name] = res
+                    else:
+                        model_name = create_model_name(mod, img_size, nb_data, epochs)
+                        res = model_full_pipeline(
+                            nb_data,
+                            epochs,
+                            mod['MODEL_FUNC'],
+                            input_shape,
+                            metrics_only = True
+                        )
+                        metrics[model_name] = res
 
     for name, eval in metrics.items():
         print(f"🎯 {name} ———————————————————————————————\n")
         print(eval)
         print('\n—————————————————————————————————————————')
+
 
     return metrics
